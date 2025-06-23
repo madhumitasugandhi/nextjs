092e09ee29e53c60573753564e2b8d9e9ae9c596
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-23 18:18:23 +0530

    UseState used in component to name the name while click the button.

--- src/components/EventsFunctionsStates.jsx
+++ src/components/EventsFunctionsStates.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const EventsFunctionsStates = () => {
+    const [firstName, setFirstName]=useState("Madhumita");
+
   const sayHello = (name) => {
     alert(`Hello ${name}`);
   };
+  const changeName = () =>{
+    setFirstName ("Madhu");
+  }
 
   return (
     <>
       <h4>Events Function & States</h4>
       <button onClick={() => sayHello('Madhu')}>Click me</button>
+      <br/>
+      <h2>My name is : {firstName}</h2>
+      <button onClick={changeName}>Change name</button>
     </>
   );
 };
