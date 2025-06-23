df05befc2a6a17b4812acdfbb6bf4976f8301e9f
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-23 18:04:10 +0530

    Event and function has been used in components.

--- src/components/EventsFunctionsStates.jsx
+++ src/components/EventsFunctionsStates.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const EventsFunctionsStates = () => {
+  const sayHello = (name) => {
+    alert(`Hello ${name}`);
+  };
+
   return (
     <>
-    <h4>
-        Events Function  & States
-    </h4>
-    <button onClick={()=> alert("Hello")}>Click me</button>
+      <h4>Events Function & States</h4>
+      <button onClick={() => sayHello('Madhu')}>Click me</button>
     </>
-  )
-}
+  );
+};
 
-export default EventsFunctionsStates
+export default EventsFunctionsStates;
