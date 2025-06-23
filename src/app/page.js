a06700283a8ed41ec2df67f048455c650b0e158d
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-23 17:52:47 +0530

    Component has been created.

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import EventsFunctionsStates from "@/components/EventsFunctionsStates";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main className={styles.main}>
        <h2>Madhumita  Sugandhi</h2>
        <p>-Done practice on how to create Next App</p>
+       <EventsFunctionsStates/>
       </main>
     </div>
   );
---
+++ src/components/EventsFunctionsStates.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EventsFunctionsStates = () => {
+  return (
+    <>
+    <h4>
+        Events Function  & States
+    </h4>
+    <button onClick={()=> alert("Hello")}>Click me</button>
+    </>
+  )
+}
+
+export default EventsFunctionsStates
