5a9e2f4a18ea376adcae1a00d5d125e47cd48b9f
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-24 14:27:02 +0530

    Common lyout practice has been done .

---
+++ src/app/contact/layout.js
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+    return (
+        <>
+            <h5>Common layout for contcat</h5>
+            {
+                children
+            }
+        </>
+    )
+}
