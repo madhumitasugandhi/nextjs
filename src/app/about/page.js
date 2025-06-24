95026821d1b3e7fb6297fa3cade863c3cd66b0e3
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-06-24 13:31:29 +0530

    Navigation routing done by creating button for routing.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,14 @@
+'use client'
+import { useRouter } from "next/navigation"
+
+
 const about = () => {
+    const router = useRouter();
   return (
     <>
         <h2>About Us</h2>
+        <button onClick={()=> router.push('/contact')}>Contact</button><br/><br/>
+        <button onClick={()=> router.push('/home')}>Home</button>
     </>
   )
 }
