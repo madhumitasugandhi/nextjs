8ea46643bcfb5a5b23f188c6bdb809aff66f843c
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-07-17 14:32:49 +0530

    Css modules practice done.

---
+++ src/app/css-modules/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CSSModules = () => {
+  return (
+    <>
+        <h2 className='main'><i>CSS Modules In Next JS</i></h2>
+    </>
+  )
+}
+
+export default CSSModules;
