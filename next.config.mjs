e394159e960ea7548c4a8b4684b0747c60656741
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-07-18 18:24:53 +0530

    Redirection concept practice.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
     images: {
   domains: ['images.pexels.com'],
 },
+redirects : async()=>{
+  return[
+    {
+      source: '/redirection',
+      destination: '/home',
+      permanent: false
+    },
+    {
+      source: '/redirection/:id',
+      destination: '/',
+      permanent: false
+    },
+  ]
+}
 };
 
 export default nextConfig;
---
+++ src/app/redirection/page.js
@@ -0,0 +1,19 @@
+'use client'
+//import React, { useState } from 'react'
+//import { redirect } from 'next/navigation';
+
+const ReDirection = () => {
+    //redirect('/home')
+    /*const [logOut, setLogOut] = useState(false);
+    if(logOut){
+        redirect('/home')
+    }*/
+  return (
+    <>
+        <h2>Redirection with next js</h2>
+        {/*<button onClick={() => setLogOut(true)}>Logout</button>*/}
+    </>
+  )
+}
+
+export default ReDirection;
