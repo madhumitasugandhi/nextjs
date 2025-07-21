cc05a68575ed2a9416f1fa363611c6f4999483ae
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-07-21 11:10:48 +0530

    GET API created with static data.

---
+++ src/db/db.js
@@ -0,0 +1,20 @@
+export const employee = [
+    {
+        empolyeeId : 1,
+        name :  'Madhumita',
+        salary : '10,000',
+        email: 'madhumitasugandhi@gamil.com'
+    },
+    {
+        empolyeeId : 2,
+        name :  'Pooja',
+        salary : '10,000',
+        email: 'pborkar28@gamil.com'
+    },
+    {
+        empolyeeId : 3,
+        name :  'Shikha',
+        salary : '10,000',
+        email: 'ssthakur@gamil.com'
+    }
+]
